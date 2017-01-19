4fc6325e5f34e5b9f07f927616a7bcd40c546de4
Robin McCorkell <robin@mccorkell.me.uk>
2017-01-19 19:46:11 +0000

    Add a function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+fn add_me(x: i32, y: i32) -> i32 {
+    x * y
+}
+
+#[test]
+fn test_add_me() {
+    assert!(add_me(2, 2) == 4);
+}
+
 fn main() {
     println!("Hello, world!");
+    println!("2 + 2 = {}", add_me(2, 2));
 }
