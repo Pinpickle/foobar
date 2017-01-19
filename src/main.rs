9ffc769b164e19d5d7d4684d9d84a90838da682e
Robin McCorkell <robin@mccorkell.me.uk>
2017-01-19 20:03:06 +0000

    Install quickcheck

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
+#[cfg(test)]
+#[macro_use]
+extern crate quickcheck;
+
 fn add_me(x: i32, y: i32) -> i32 {
     x * y
-}
-
-#[test]
-fn test_add_me() {
-    assert!(add_me(2, 2) == 4);
 }
 
 fn main() {
     println!("Hello, world!");
     println!("2 + 2 = {}", add_me(2, 2));
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_add_me() {
+        use super::add_me;
+        assert!(add_me(2, 2) == 4);
+    }
+
+    quickcheck! {
+        fn prop(x: i32, y: i32) -> bool {
+            use super::add_me;
+            add_me(x, y) == x + y
+        }
+    }
 }
